cc9cd53ac065691520f7b9f680d11a14660ce272
code <43993689+Ryanyuanao@users.noreply.github.com>
2020-04-01 14:22:04 +0800

    Add files via upload

---
+++ js/click_show_text.js
@@ -0,0 +1,3 @@
+// build time:Mon Mar 23 2020 01:55:10 GMT+0000 (Coordinated Universal Time)
+var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(t){var e=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var o=a("<span/>").text(e[a_idx]);a_idx=(a_idx+1)%e.length;var n=t.pageX,i=t.pageY;o.css({"z-index":5,top:i-20,left:n,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"});a("body").append(o);o.animate({top:i-180,opacity:0},3e3,function(){o.remove()})});setTimeout("delay()",2e3)});function delay(){$(".buryit").removeAttr("onclick")}
+//rebuild by neat 
