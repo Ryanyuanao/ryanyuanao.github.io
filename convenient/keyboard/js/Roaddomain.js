f488b64e7a54b3f410a4d5f6d0307262234f5151
code <43993689+Ryanyuanao@users.noreply.github.com>
2020-04-02 13:14:16 +0800

    Delete Roaddomain.js

--- convenient/keyboard/js/Roaddomain.js
+++
@@ -1,7 +0,0 @@
-        if (window.location.href.indexOf("www.guolibo.com") > -1) {
-        document.write("<div style='float:left;'><a href='http://www.miitbeian.gov.cn/' target='_blank'>沪ICP备12023051号</a></div><div style='float:left;'><img src='./img/ghs.png'/></div><div style='float:left;'><a href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011302002333' target='_blank'>沪公网安备31011302002333号</a></div>");
-        } else if (window.location.href.indexOf("www.yomean.com") > -1) {
-        document.write("<div style='float:left;'><a href='http://www.miitbeian.gov.cn/' target='_blank'>沪ICP备09034776号</a></div><div style='float:left;'><img src='./img/ghs.png'/></div><div style='float:left;'><a href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011502007042' target='_blank'>沪公网安备31011502007042号</a></div>");
-		} else if (window.location.href.indexOf("") > -1) {
-        document.write("<div style='float:left;'>FBI不给提供</div><div style='float:left;'><img src='./img/ghs.png'/></div><div style='float:left;'>CIA也不给提供</div>");
-        }
